8db5a92e09e288fed0c37efb258ee5b81fec88a4
krishnadev <krishnadevkichus@gmail.com>
2024-10-08 12:40:27 +0530

    added buildtree and root function

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -1,44 +1,41 @@
 class Node{
-    constructor(data = null , leftNode = null , rightNode = null){
+    constructor(data = null , left = null , right = null){
         this.data = data
-        this.leftNode = leftNode
-        this.rightNode
+        this.left = left
+        this.right = right
     }
 }
 
 class Tree{
     constructor(){
         this.treeArr = []
-        this.root = buildTree()
-    }
-
-    sortArr(arr){
-        for(let i=0;i<arr.length;i++){
-            for(let j=0;j<arr.length-1;j++){
-                if (arr[j]>arr[j+1]){
-                    let temp = arr[j+1]
-                    arr[j+1] = arr[j]
-                    arr[j] = temp
-                }
-            }
-        }
-        return arr
+        this.root = null
     }
 
     dupliArr(arr){
-        for(let i=0;i<arr.length;i++){
-            for(let j=0;j<arr.length-1;j++){
-                if (arr[i]==arr[j]){
-                    arr.splice(j,1)
-                    j--
-                }
-            }
-        }
-        return arr
+        return [...new Set(arr)]
     }
 
-    buildTree(array){
-        array = this.sortArr(array)
-        array = this.dupliArr(array)
+    processArr(arr){
+        let uniqArr = this.dupliArr(arr)
+        return uniqArr.sort()
     }
+
+    buildTreeRoot(array){
+        let processedArr = this.processArr(array)
+        this.root = createRoot(array , 0 , array.length-1)
+        return this.root
+    }
+
+    createRoot(array , start , end){
+        if(start>end) return null
+
+        const mid = Math.floor((start+end)/2)
+        const newNode = new Node(array[mid])
+        newNode.left = this.createRoot(array , start , mid-1)
+        newNode.right = this.createRoot(array , mid+1 , end)
+        return newNode
+    }
+
+
 }
