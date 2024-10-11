834e04749940a04a9cfee04c375f90bb0c4b6eb5
krishnadev <krishnadevkichus@gmail.com>
2024-10-11 18:56:40 +0530

    added height function

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -8,7 +8,6 @@ class Node {
 
 class Tree {
   constructor() {
-    this.treeArr = [];
     this.root = null;
   }
 
@@ -18,12 +17,12 @@ class Tree {
 
   processArr(arr) {
     let uniqArr = this.dupliArr(arr);
-    return uniqArr.sort();
+    return uniqArr.sort((a, b) => a - b); 
   }
 
   buildTreeRoot(array) {
     let processedArr = this.processArr(array);
-    this.root = createRoot(array, 0, array.length - 1);
+    this.root = this.createRoot(processedArr, 0, processedArr.length - 1);
     return this.root;
   }
 
@@ -39,6 +38,11 @@ class Tree {
 
   insert(value) {
     const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode; 
+      return;
+    }
+
     let currentVal = this.root;
     while (true) {
       if (value >= currentVal.data) {
@@ -61,34 +65,125 @@ class Tree {
 
   deleteItem(value) {
     this.root = this.deleteNode(this.root, value);
-}
+  }
 
-deleteNode(node, value) {
+  deleteNode(node, value) {
+    if (node === null) return null;
+
     if (value < node.data) {
-        node.left = this.deleteNode(node.left, value);
+      node.left = this.deleteNode(node.left, value);
     } else if (value > node.data) {
-        node.right = this.deleteNode(node.right, value);
+      node.right = this.deleteNode(node.right, value);
     } else {
-        if (node.left === null) {
-            return node.right; 
-        } else if (node.right === null) {
-            return node.left; 
-        }
-        const minLargerNode = this.getMinNode(node.right);
-
-        node.data = minLargerNode.data;
-
-        node.right = this.deleteNode(node.right, minLargerNode.data);
+      if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+      const minLargerNode = this.getMinNode(node.right);
+      node.data = minLargerNode.data; 
+      node.right = this.deleteNode(node.right, minLargerNode.data); 
     }
-    return node; 
-}
-
-
-getMinNode(node) {
-    while (node.left !== null) {
-        node = node.left; 
     return node;
-}
+  }
 
-}
+  getMinNode(node) {
+    while (node.left !== null) {
+      node = node.left; 
+    }
+    return node;
+  }
+
+  find(value) {
+    let currentVal = this.root;
+    while (currentVal) {
+      if (value > currentVal.data) {
+        currentVal = currentVal.right;
+      } else if (value < currentVal.data) {
+        currentVal = currentVal.left;
+      } else {
+        return currentVal; 
+      }
+    }
+    return null; 
+  }
+
+  levelOrder(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('A callback is required');
+    }
+
+    const queue = [];
+    if (this.root) {
+      queue.push(this.root);
+    }
+
+    while (queue.length > 0) {
+      const currentNode = queue.shift(); 
+      callback(currentNode); 
+
+     
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+  }
+
+  inOrder(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('A callback is required');
+    }
+
+    const traverse = (node) => {
+      if (node == null) return;
+      traverse(node.left);
+      callback(node); 
+      traverse(node.right);
+    };
+
+    traverse(this.root);
+  }
+
+  preOrder(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('A callback is required');
+    }
+
+    const traverse = (node) => {
+      if (node == null) return;
+      callback(node);
+      traverse(node.left);
+      traverse(node.right);
+    };
+
+    traverse(this.root);
+  }
+
+  postOrder(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('A callback is required');
+    }
+
+    const traverse = (node) => {
+      if (node == null) return;
+      traverse(node.left);
+      traverse(node.right);
+      callback(node); 
+    };
+
+    traverse(this.root);
+  }
+
+
+  height(node) {
+    if (node === null) {
+      return -1;
+    }
+    const leftHeight = this.height(node.left);
+    const rightHeight = this.height(node.right);
+    return 1 + Math.max(leftHeight, rightHeight);
+  }
 }
